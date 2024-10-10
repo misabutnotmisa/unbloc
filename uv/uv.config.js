3a0d94624bfcd08843211f762029cf0b57c4103e
misa <fredrigo991@gmail.com>
2024-10-10 18:07:36 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://replit.com/@vincedoes/DM-unbl0cker-bare-server',
+    bare: 'https://novaa-psi.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
